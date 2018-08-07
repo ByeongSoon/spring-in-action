cba4ba44eab5ce45d71568398ecf6d7fbc69bc18
Byeongsoon Jang <byeongsoon@wisoft.io>
2018-08-07 23:09:56 +0900

    2장 예제 작성 중

---
+++ 02-bean-wiring/src/main/java/soundsystem/SgtPeppers.java
@@ -0,0 +1,18 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+//import javax.inject.Named;
+
+@Component("lonelyHeartsClub")
+//@Named("lonelyHeartsClub")
+public class SgtPeppers implements CompactDisc {
+
+  private String title = "Sgt, Pepper's Lonely Hearts Club Band";
+  private String artist = "The Beatles";
+
+  @Override
+  public void play() {
+    System.out.println("Playing " + title + " by " + artist);
+  }
+
+}
